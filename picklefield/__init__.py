c0876dc8807648041b10befe97abc7ad51d34c6e
Marcin Nowak <marcin.j.nowak@gmail.com>
2021-03-30 15:17:50 +0200

    update version

--- picklefield/__init__.py
+++ picklefield/__init__.py
@@ -5,6 +5,6 @@ from .fields import PickledObjectField
 
 __all__ = 'VERSION', '__version__', 'DEFAULT_PROTOCOL', 'PickledObjectField'
 
-VERSION = (3, 0, 1, 'final', 0)
+VERSION = (3, 0, 2, 'dev', 1)
 
 __version__ = str(packaging.version.Version('.'.join(str(p) for p in VERSION)))
